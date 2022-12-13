f1546c8c62b0921b70ebc5dc2d124054fcfe651e
ersachingoyal <goyalsachin176@gmail.com>
2022-12-13 17:57:41 +0530

    setup basic sequqlize to create a db

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const setupAndStartServer = async() =>{
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
-    
+     
     app.listen(PORT, () =>{
         console.log("server started at port" ,PORT);
         // console.log(process.env.PORT)
