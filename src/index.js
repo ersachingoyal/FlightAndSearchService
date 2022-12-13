19f82687ab1faa943352b91e2cd9b466698d4ea5
ersachingoyal <goyalsachin176@gmail.com>
2022-12-13 17:15:58 +0530

    added the body parser

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 // require('dotenv').config();  -> we will move this process in config folder
 const { PORT } = require('./config/serverConfig')
+const bodyParser = require('body-parser');
 
 
 const setupAndStartServer = async() =>{
@@ -8,6 +9,10 @@ const setupAndStartServer = async() =>{
     //creating the express object
     //const PORT = 3000;  // we will move these all sensitive info like this one and other like pass into env variables
     const app = express();
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+    
     app.listen(PORT, () =>{
         console.log("server started at port" ,PORT);
         // console.log(process.env.PORT)
