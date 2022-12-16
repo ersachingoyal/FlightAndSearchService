eb8969d0dea2e46cf1ed612ac9097912e89d4e0c
Sachin goyal <goyalsachin176@gmail.com>
2022-12-16 13:17:52 +0530

    completed city repo

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,3 +1,5 @@
+// repo file is used to interact with models , it exposes all the functions related to our model table
+
 const { City } = require('../models/index');   // got access to the model to interact with the table
 
 class CityRepository{
@@ -6,6 +8,7 @@ class CityRepository{
             const city = await City.create({ name }); // to create an entry into the table
             return city;
         } catch (error) {
+            console.log("Something went wrong");
             throw { error }
         }
     }
@@ -17,7 +20,41 @@ class CityRepository{
                     id: cityId
                 }
             })
+            return true;
         } catch (error) {
+            console.log("Something went wrong");
+            throw { error }
+        }
+    }
+
+    async updateCity(cityId, data) {  //cityid to match which city to update and the data is the param which is going to update
+        try {
+            // The below approach also works but will not return updated object
+            // if we are using PgSql then returning: true can be used, else not
+            // const city = await City.update(data, {
+            //     where: {
+            //         id: cityId
+            //     },
+            //      
+            // });
+            
+            // for getting updated data in mysql we use the below approach
+            const city = await City.findByPk(cityId);
+            city.name = data.name;
+            await city.save();
+            return city;
+        } catch (error) {
+            console.log("Something went wrong");
+            throw { error }
+        }
+    }
+
+    async getCity(cityId) {
+        try {
+            const city = await City.findByPk(cityId); //coz id is a primary key, so findbypk directly
+            return city;
+        } catch (error) {
+            console.log("Something went wrong");
             throw { error }
         }
     }
